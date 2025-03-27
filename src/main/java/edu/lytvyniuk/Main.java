d659ac3b337337072aeaf2a2ce27856ddddeb718
darynkka <dashalitviniuk20@gmail.com>
2025-03-27 18:43:00 +0200

    initial commit

---
+++ src/main/java/edu/lytvyniuk/Main.java
@@ -0,0 +1,19 @@
+package edu.lytvyniuk;
+
+/*
+  @author darin
+  @project Default (Template) Project
+  @class ${NAME}
+  @version 1.0.0
+  @since 27.03.2025 - 18.41
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
